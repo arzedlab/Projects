724997ef6189779f97cecdb49b2e53dcb477ae7b
arzedlab <arzedlab@gmail.com>
2021-06-18 15:43:54 +0500

    Prime Factorization

--- Numbers/Prime Factorization/main.py
+++ Numbers/Prime Factorization/main.py
@@ -16,6 +16,20 @@ def prime_factors(n):
     if n > 2:
         print(n)
 
+def prime_factors_list(n):
+    out = list()
+    for num in range(1, n+1):
+        prime = True
+        for i in range(2, num):
+            if (num % i == 0):
+                prime = False
+        if prime:
+            out.append(num)
+    return out
+
 if __name__ == '__main__':
     num = int(input('Enter num:'))
-    prime_factors(num)
+    # prime_factors(num)
+    list = prime_factors_list(num)
+    for element in list:
+        print(element)
